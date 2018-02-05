8696547f9fea68312beec989a5cb4c7de01b0b90
Jennifer Anne Hayek <jenniferanne.hayek@gmail.com>
2018-02-05 02:23:26 +0000

    Form Validataion Step, major cleanup of HTML/CSS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,46 +3,39 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-//**********************************************************************************/
-$(function() {
-  $(".new-tweet form ").on('submit', function(event){
-    event.preventDefault();
-    const data = $(this).serialize();
-    $.post("/tweets", data)
+// *ction() {
+// // ****************$(f*****************************************************************/
+// un  $(".new-tweet form ").on('submit', function(event){
+// //     event.preventDefault();
+//     const data = $(this).serialize();
+//     $.post("/tweets", data)
 
-  });
-})
+//   });
+// })
 
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": {
+//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//     },
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// }
 
 
 //
 $(document).ready(function() {
-  // var $tweet = createTweetElement(tweetData);
-  //renderTweets(data);
-  // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-
-  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
   
-  //Form Validataion Exercise
   function checkTweetContent(){
     if(($('#tweetText').val()==null) || ($('#tweetText').val()=="")){
       alert("The tweet text box is emty");
@@ -60,9 +53,10 @@ $(document).ready(function() {
       method: 'GET',
       url: '/tweets',
       success: function(result){
-        console.log("the ajax call was successful");
-        console.log(result); 
+        
+        
         renderTweets(result);
+      
         //create a function to render tweets
       },
       error: function(error){
@@ -70,10 +64,32 @@ $(document).ready(function() {
         console.log(error);
       }
     });
+  }
+
+    function ComposeTweet() {
+      $('form').on('submit', (event) => {
+        console.log("form click");
+
+      event.preventDefault();
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        text: $('textarea').val(),
+        success: function(result){
+          loadTweets();
+          // renderTweets(result);
+          //create a function to render tweets
+        },
+        error: function(error){
+          console.log("there was an error in making ajax cal composel");
+          console.log(error);
+        }
+      });
+      });
  
   }
   function renderTweets(tweets) {
-    console.log("render-tweets")
+  
     // loops through tweets
     for(var i = 0; i < tweets.length; i++){
       createTweetElement(tweets[i]); 
@@ -91,6 +107,7 @@ $(document).ready(function() {
   }
 
   function createTweetElement (tweet)  {
+    console.log("tweet", tweet);
     console.log("inside createtweetelement", tweet.user.name)
   
     var tweetContainer = $("section#tweets-container");
@@ -105,8 +122,9 @@ $(document).ready(function() {
     // return $tweet;
   }
 
-  $('.new-tweet').hide();
+  // $('.new-tweet').hide();
   loadTweets();
+  ComposeTweet();
 
 
 
@@ -116,65 +134,3 @@ $(document).ready(function() {
   
 });
 
-//**************************************************************************************** */
-// Fake data taken from tweets.json
-// const data = [
-//    {
-//   "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//  {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
-
-//renderTweets(data);
-
-// function createTweetElement (tweet)  {
-  
-//   var $tweet = $("<article>").addClass("tweet");
-//   var $header = $("<header>").text(tweet.user.name);
-//   $tweet.append($header)
-//   var tweetContent = $("<p>").text(tweet.content.text);
-//   $tweet.append(tweetContent)
-    
-//   return $tweet;
-// }
